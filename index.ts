05e81ec6878bfc3e8f2bc9732dfb261ad45d3d24
FraterSKS <jackson.a.furr@gmail.com>
2022-11-28 17:46:10 -0500

    login cookies

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 import express, { Express, Request, Response } from "express";
-import mongoose from "mongoose";
+import mongoose, { Mongoose, Model } from "mongoose";
 import morgan from "morgan";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -10,19 +10,26 @@ import path from "path";
 import bcrypt from "bcrypt";
 
 //MONGO
+import { IUser } from "./models/User";
 import "./models/User";
-const User = mongoose.model("User");
+const User: Model<IUser> = mongoose.model<IUser>("User");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 
 process.env.MONGO_URI && mongoose.connect(process.env.MONGO_URI);
 
-app.use(cors());
+app.use(cors({credentials: true}));
 app.use(express.json());
 app.use(morgan("tiny"));
 
 //SESSION
+declare module "express-session" {
+  interface SessionData {
+    user_id: string;
+  }
+}
+
 process.env.SESSION_SECRET &&
   app.use(
     session({
@@ -36,6 +43,12 @@ process.env.SESSION_SECRET &&
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
+//TYPES
+type userInfo = {
+  username: string;
+  password: string;
+};
+
 //ROUTES
 // app.get("/*", (req, res): void => {
 //   res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
@@ -44,12 +57,10 @@ app.use(express.static(path.join(__dirname, "..", "client", "build")));
 app.post(
   "/register",
   async (req, res): Promise<Response<any, Record<string, any>>> => {
-    const { username, password } = req.body;
-    const check = await User.findOne({ username });
+    const { username, password }: userInfo = req.body;
+    const check: Model<IUser> | any = await User.findOne({ username });
     if (check) {
       return res.json("Taken");
-    } else {
-      return res.json("Success");
     }
 
     const hash: string = await bcrypt.hash(password, 12);
@@ -59,13 +70,30 @@ app.post(
     });
 
     await newUser.save();
-    return res.json("Succesfully registered");
+
+    req.session.user_id = newUser._id;
+    return res.json("Succesfully registered: " + req.session.user_id);
   }
 );
 
-app.post("/login", (req, res): void => {
-  console.log("Success");
-});
+app.post(
+  "/login",
+  async (req, res): Promise<Response<any, Record<string, any>>> => {
+    const { username, password }: userInfo = req.body;
+    const userQuery: Model<IUser> | any = await User.findOne({ username });
+    if (!userQuery) {
+      return res.json("Invalid");
+    }
+
+    const checkPassword: boolean = await bcrypt.compare(password, userQuery.password);
+    if (checkPassword) {
+      req.session.user_id = userQuery._id;
+      return res.json("Logged in: " + req.session.user_id);
+    } else {
+      return res.json("Invalid");
+    }
+  }
+);
 
 app.listen(PORT, (): void => {
   console.log(`Listening on ${PORT}...`);
--- models/User.ts
+++ models/User.ts
@@ -1,10 +1,15 @@
-import mongoose from "mongoose";
+import { Document, Schema, Model, model } from "mongoose";
 
-const User = new mongoose.Schema({
+export interface IUser extends Document {
+  username: string;
+  password: string;
+}
+
+const UserSchema = new Schema<IUser>({
   username: String,
   password: String,
 });
 
-mongoose.model("User", User);
+model<IUser>("User", UserSchema);
 
 export {};
