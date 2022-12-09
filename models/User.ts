d559f125c5b3de21fd6a7808e3b5dd374375b587
FraterSKS <jackson.a.furr@gmail.com>
2022-12-08 20:34:18 -0500

    user info model

--- models/User.ts
+++ models/User.ts
@@ -3,11 +3,17 @@ import { Document, Schema, Model, model } from "mongoose";
 export interface IUser extends Document {
   username: string;
   password: string;
+  firstName?: string;
+  lastName?: string;
+  location?: string;
 }
 
 const UserSchema = new Schema<IUser>({
   username: String,
   password: String,
+  firstName: String,
+  lastName: String,
+  location: String,
 });
 
 model<IUser>("User", UserSchema);
